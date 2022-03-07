967663b41299031eb600352ba1b340112e082e3b
Matt Harvey <mattharvey@google.com>
2022-03-07 12:39:41 -0800

    Bare-bones model fitting

--- multispecies_whale_detection/scripts/train.py
+++ multispecies_whale_detection/scripts/train.py
@@ -36,6 +36,7 @@ from absl import flags
 import tensorflow as tf
 
 from multispecies_whale_detection import dataset
+from multispecies_whale_detection import front_end
 
 FLAGS = flags.FLAGS
 
@@ -43,6 +44,19 @@ flags.DEFINE_string('base_dir', None,
                     'Base directory with input/ and output/ subdirectories.')
 
 CLASS_NAMES = ['Orca', 'SRKW']
+
+
+def preprocess(spectrogram):
+  # Keras applications EfficientNet expects input in [0.0, 255.0].
+  low_limit_db = -6.0
+  high_limit_db = 90.0
+  low_limit_model = 0.0
+  high_limit_model = 255.0
+  scaled = ((high_limit_model - low_limit_model) *
+            (spectrogram - low_limit_db) / (high_limit_db - low_limit_db))
+  # Adds RGB color channel dimension where all are equal.
+  fake_image = tf.tile(tf.expand_dims(scaled, -1), [1, 1, 1, 3])
+  return fake_image
 
 
 def main(argv: Sequence[str]) -> None:
@@ -68,11 +82,26 @@ def main(argv: Sequence[str]) -> None:
       min_overlap=0.25,
   ).batch(batch_size).prefetch(1)
 
-  for batch in validation_dataset:
-    features, labels = batch
-    if tf.reduce_any(labels > 0.0):
-      print(batch)
-      break
+  model = tf.keras.Sequential([
+      front_end.Spectrogram(),
+      tf.keras.layers.Lambda(preprocess),
+      tf.keras.applications.EfficientNetB0(
+          include_top=False,
+          weights=None,
+          pooling='max',
+      ),
+      tf.keras.layers.Dense(len(CLASS_NAMES), activation='sigmoid'),
+  ])
+  model.compile(
+      optimizer='adam',
+      loss=tf.keras.losses.BinaryCrossentropy(from_logits=False),
+  )
+
+  model.fit(
+      train_dataset,
+      validation_data=validation_dataset,
+      epochs=10,
+  )
 
 
 if __name__ == '__main__':
